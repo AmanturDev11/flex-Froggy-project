73a6f3af8c15e603a86c68b0c17c5ef6084f2abf
AmanturDev11 <ismailovamantur22@gmail.com>
2024-06-18 03:28:22 +0600

    flex-Froggy-project

--- src/components/flexFroggy/FlexFroggy.tsx
+++ src/components/flexFroggy/FlexFroggy.tsx
@@ -1,14 +1,41 @@
+// import React from "react";
+// import { DataFroggy } from "../../utils/data";
+
+// const FlexFroggy = () => {
+// 	return (
+// 		<div>
+// 			FlexFroggy
+// 			{DataFroggy.map((item) => (
+// 				<div key={item.id}>
+// 					<p>{item.text}</p>
+// 					{/* <div>{item.instruction}</div> */}
+// 					<p>{item.example}</p>
+// 				</div>
+// 			))}
+// 		</div>
+// 	);
+// };
+
+// export default FlexFroggy;
 import React from "react";
 import { DataFroggy } from "../../utils/data";
 
 const FlexFroggy = () => {
 	return (
 		<div>
-			FlexFroggy
+			<h1>FLEXBOX FROGGY</h1>
 			{DataFroggy.map((item) => (
 				<div key={item.id}>
 					<p>{item.text}</p>
-					{/* <div>{item.instruction}</div> */}
+					{item.instruction.map((instr, index) => (
+						<div key={index}>
+							<p>{instr.сondition1}</p>
+							<p>{instr.сondition2}</p>
+							<p>{instr.сondition3}</p>
+							<p>{instr.сondition4}</p>
+							<p>{instr.сondition5}</p>
+						</div>
+					))}
 					<p>{item.example}</p>
 				</div>
 			))}
